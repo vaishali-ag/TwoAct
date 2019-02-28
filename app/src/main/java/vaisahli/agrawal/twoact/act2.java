f14e85445e5ecef18615a2039a3e94d3ab0a6bd6
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-28 10:17:24 +0530

    add secnd activity

---
+++ app/src/main/java/vaisahli/agrawal/twoact/act2.java
@@ -0,0 +1,13 @@
+package vaisahli.agrawal.twoact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class act2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2);
+    }
+}
